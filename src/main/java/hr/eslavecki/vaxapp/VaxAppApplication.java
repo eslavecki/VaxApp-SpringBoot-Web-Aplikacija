93452fdce8ae23498ab8ae7766ad0f9e0a3afa20
Emanuel Slavečki <emanuel.slavecki@gmail.com>
2022-01-30 23:04:51 +0100

    innit commit

---
+++ src/main/java/hr/eslavecki/vaxapp/VaxAppApplication.java
@@ -0,0 +1,13 @@
+package hr.eslavecki.vaxapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VaxAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VaxAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hr/eslavecki/vaxapp/VaxAppApplicationTests.java
@@ -0,0 +1,13 @@
+package hr.eslavecki.vaxapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VaxAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
