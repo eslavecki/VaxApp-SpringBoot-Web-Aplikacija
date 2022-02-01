4d0e33753c914570fce205b5cb0fc7188faa8758
Emanuel Slavečki <emanuel.slavecki@gmail.com>
2022-02-01 15:33:54 +0100

    Hello World output

--- src/main/java/hr/eslavecki/vaxapp/VaxAppApplication.java
+++ src/main/java/hr/eslavecki/vaxapp/VaxAppApplication.java
@@ -8,7 +8,7 @@ public class VaxAppApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(VaxAppApplication.class, args);
-        System.out.println("Hello World");
+        System.out.println("Hello World Test");
     }
 
 }
